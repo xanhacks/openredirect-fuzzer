c29f6f67b455eeb5cf3e55bc10e2a5d340c6f1e3
xanhacks <xanhacks@protonmail.com>
2023-01-11 15:58:14 +0100

    add header and error management

--- fuzzer.py
+++ fuzzer.py
@@ -5,18 +5,26 @@ from urllib3 import disable_warnings
 from urllib3.exceptions import InsecureRequestWarning
 
 from requests import get
-from requests.exceptions import InvalidSchema
+from requests.exceptions import InvalidSchema, ConnectionError
 
 
 disable_warnings(InsecureRequestWarning)
 
 
-def fuzz(url, wordlist_path, cookies, http_proxy):
+def fuzz(url, wordlist_path, cookies, http_proxy, header):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"
     }
     proxies = {}
 
+    if header:
+        if "=" not in header:
+            print("=> Wrong header format, see help")
+            return
+        key = header.split("=")[0]
+        value = "".join(header.split("=")[1:])
+        headers[key] = value
+        print("=> Headers :", headers)
     if cookies:
         headers["Cookie"] = cookies
     if http_proxy:
@@ -45,7 +53,10 @@ def fuzz(url, wordlist_path, cookies, http_proxy):
                 else:
                     print(f"=> No redirection - {req.status_code} | URL : {req.url}\n")
             except InvalidSchema:
-                print(f"=> Error - Invalid schema\n")
+                print("=> Error - Invalid schema\n")
+            except ConnectionError as err:
+                print("=> Error - Unable make the HTTP request")
+                print(err)
             except KeyboardInterrupt:
                 print("\nExiting ...")
                 break
@@ -54,14 +65,17 @@ def fuzz(url, wordlist_path, cookies, http_proxy):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="OpenRedirect Fuzzer")
 
-    parser.add_argument("-u", "--url", help="URL to attack (replace target parameter value by FUZZ).")
-    parser.add_argument("-w", "--wordlist", help="Path to payload list file.")
+    parser.add_argument("-u", "--url", required=True,
+                        help="URL to attack (replace target parameter value by FUZZ).")
+    parser.add_argument("-w", "--wordlist", required=True,
+                        help="Path to payload list file.")
     parser.add_argument("-c", "--cookies", help="Add cookies to all the requests.")
     parser.add_argument("-p", "--proxy", help="Set a proxy to all the requests.")
+    parser.add_argument("-H", "--header", help="Add an HTTP header (ex: -H 'User-Agent=Mozilla 4.0...').")
 
     args = parser.parse_args()
 
     original_domain = urlparse(args.url).netloc
     print("Start fuzzing ...")
-    fuzz(args.url, args.wordlist, args.cookies, args.proxy)
+    fuzz(args.url, args.wordlist, args.cookies, args.proxy, args.header)
 
